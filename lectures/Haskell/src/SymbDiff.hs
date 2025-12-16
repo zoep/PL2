d38722095e7813a9dda37432573a4a22e6b8105b
zoep <zoe.paraskevopoulou@gmail.com>
2025-12-16 21:56:46 +0200

    Nits

--- lectures/Haskell/src/SymbDiff.hs
+++ lectures/Haskell/src/SymbDiff.hs
@@ -14,36 +14,24 @@ data Expr
 
 diff :: Expr -> String -> Expr
 diff (Const _) _ = Const 0
-diff (Var x) var
-    | x == var  = Const 1
-    | otherwise = Const 0
-diff (Add u v) var = Add (diff u var) (diff v var)
-diff (Sub u v) var = Sub (diff u var) (diff v var)
-diff (Mul u v) var = Add (Mul (diff u var) v) (Mul u (diff v var))
-diff (Pow u n) var = Mul (Mul (Const (fromIntegral n)) (Pow u (n - 1))) (diff u var)
+diff (Var x) y | x == y = Const 1
+               | otherwise = Const 0
+diff (Add e1 e2) x = Add (diff e1 x) (diff e2 x)
+diff (Sub e1 e2) x = Sub (diff e1 x) (diff e2 x)
+diff (Mul e1 e2) x = Add (Mul e1 (diff e2 x)) (Mul (diff e1 x) e2)
+diff (Pow e1 n) x = Mul (Const (fromIntegral n)) (Mul (Pow e1 (n-1)) (diff e1 x))
 
 
-expr1 :: Expr
-expr1 = Add (Pow (Var "x") 3)
-            (Mul (Const 2) (Var "x"))
-
--- >>> diff expr1 "x" 
--- Add (Mul (Mul (Const 3.0) (Pow (Var "x") 2)) (Const 1.0)) (Add (Mul (Const 0.0) (Var "x")) (Mul (Const 2.0) (Const 1.0)))
-
-
-eval :: [(String, Double)] -> Expr -> Double
-eval _ (Const c) = c
-eval env (Var x) = case lookup x env of
-                        Just v  -> v
-                        Nothing -> error ("Variable " ++ x ++ " not found in environment")
-eval env (Add u v) = eval env u + eval env v
-eval env (Sub u v) = eval env u - eval env v
-eval env (Mul u v) = eval env u * eval env v
-eval env (Pow u n) = eval env u ** fromIntegral n
-
--- >>> eval [("x", 2)] expr1
--- 12.0
-
+eval :: Expr -> [(String, Double)] -> Double
+eval (Const n) _ = n
+eval (Var x) env = 
+    case lookup x env of
+        Just v -> v
+        Nothing -> error $ "Variable " <> x <> " not found"
+eval (Add e1 e2) env = eval e1 env + eval e2 env
+eval (Sub e1 e2) env = eval e1 env - eval e2 env
+eval (Mul e1 e2) env = eval e1 env * eval e2 env
+eval (Pow e1 n) env = eval e1 env ** fromIntegral n
 
 approxRoot :: Expr -> String -> Double -> [Double]
 approxRoot f var x = x:approxRoot f var next
@@ -52,22 +40,18 @@ approxRoot f var x = x:approxRoot f var next
     f'x = eval [(var, x)] (diff f var)
     next = x - fx / f'x
 
-sqRoot :: Double -> Double
-sqRoot y = head . dropWhile notClose $ approxRoot (Sub (Pow (Var "x") 2) (Const y)) "x" 1
+
+sqroot :: Double -> Double
+sqroot y = head $ dropWhile cond (approx f "x" 1)
   where
-    notClose x = abs (x*x - y) > 1e-12
+    cond x = abs (x * x - y) > 1e-12
+    f = Sub (Pow (Var "x") 2) (Const y)
 
--- >>> sqRoot 9     
--- 3.0
-
--- >>> sqRoot 25
--- 5.0
-
--- >>> sqRoot 42
--- 6.48074069840786
 
 
 {- 
+
+-- Alternative implementation with iteration limit and error threshold, without an infinite list:
 
 approxRoot :: Expr -> String -> Double -> Double -> Double -> [Double]
 approxRoot f var x = guess :: approxRoot' f var guess erro
